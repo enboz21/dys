de33453c261f0189c424951610e7d699ba97736d
enboz21 <enesb33@outlook.com>
2024-06-30 21:30:54 +0300

    tasarım

--- dys/src/main/java/dys/sys/HelloController.java
+++ dys/src/main/java/dys/sys/HelloController.java
@@ -1,14 +1,22 @@
 package dys.sys;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Label tarih;
+    @FXML
+    private Button seç_1;
+    @FXML
+    private Button seç_2;
+    @FXML
+    private Button yedek;
+    @FXML
+    private Button kur;
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
